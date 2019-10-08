ab521d7e14ea7c920374f11f8eb58346c28b6a0e
tonykosmos <a.kosmovskiy@gmail.com>
2019-10-08 17:26:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let arr1 = first.split('').reverse();
+  let arr2 = second.split('').reverse();
+  let sums = [];
+  for (let i = 0; i < arr1.length; i++){
+    for (let j = 0; j < arr2.length; j++){
+      let mul = arr1[i] * arr2[j];
+      if (sums[i + j]) {
+        sums[i + j] = sums[i + j] + mul;
+      }
+      else sums[i + j] = mul;
+    }
+  }
+  for (let i = 0; i < sums.length; i++) {
+    let units = sums[i] % 10;
+    let decimal = Math.floor (sums[i] / 10);
+    sums[i] = units;
+    if (sums[i+1]){
+      sums[i+1] += decimal;
+    }
+    else if (decimal !=0) {
+      sums[i+1] = decimal;
+    }
+  }
+  return sums.reverse().join('');
 }
